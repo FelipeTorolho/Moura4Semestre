93c2abcbafd55552e1afbd65b26bf29b2ad1a722
FELIPE TOROLHO <42400772819@info.local>
2026-08-12 09:16:53 -0300

    Feito mais uma parte do ReactListNative

--- reactnative/ReactListNative/src/components/taskitem/TaskItem.jsx
+++ reactnative/ReactListNative/src/components/taskitem/TaskItem.jsx
@@ -1,12 +1,21 @@
-import { Text, View } from "react-native"
-import{ TaskItemStyle } from "./TaskItemStyle"
+import { Text, View, TouchableOpacity } from "react-native"
+import { Ionicons } from "@expo/vector-icons"
+import { TaskItemStyle } from "./TaskItemStyle"
 
 export const TaskItem = () => {
   return (
     <View style={TaskItemStyle.cardBox}>
-        <Text style={TaskItemStyle.textBox}>TaskItem Component</Text>
-    </View>
+        <Text style={TaskItemStyle.textBox}>Estudar React Native</Text>
 
-    
+        <View style={TaskItemStyle.iconsContainer}>
+            <TouchableOpacity style={TaskItemStyle.iconButton} onPress={() => {}}>
+                <Ionicons name="create-outline" size={18} color="#9BB8ED" />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={TaskItemStyle.iconButtonTrash} onPress={() => {}}>
+                <Ionicons name="trash-outline" size={18} color="#D46E75" />
+            </TouchableOpacity>
+        </View>
+    </View>
   )
 }
